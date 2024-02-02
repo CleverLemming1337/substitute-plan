69f270a751302b8e5c56bb0c24abed3e8b5eaa72
CleverLemming <disneysbhoernchen@outlook.de>
2024-02-02 14:36:28 +0100

    Flask server working

--- backend/http-server.py
+++ backend/http-server.py
@@ -1,9 +1,7 @@
-import database
 from flask import Flask, request, jsonify
+import database
 
 app = Flask(__name__)
-connection = database.newConnection()
-cursor = database.newCursor(connection)
 
 @app.route('/', methods=['GET'])
 def redirect_to_subst():
@@ -11,25 +9,30 @@ def redirect_to_subst():
 
 @app.route('/subst', methods=['GET'])
 def get_subst():
+    connection = database.newConnection()
+    cursor = database.newCursor(connection)
     x_version = request.headers.get('X-version')
     if x_version == '1':
         dbresponse = database.getAll(cursor)
         return jsonify(dbresponse), 200
     elif x_version == '2':
         dbresponse = database.getAll(cursor)
-        keys = "id date class lesson teacher room notes".split()
-        response_dict = {k:dbresponse[index] for index, k in enumerate(keys)}
-        return jsonify(response_dict), 200
+        print(dbresponse)
+        keys = "id date class lesson subject teacher room notes".split()
+        response = [{k:e[index] for index, k in enumerate(keys)} for e in dbresponse] # e stands for entry and k for key
+        return jsonify(response), 200
     else:
         return '', 400
 
 @app.route('/subst', methods=['POST'])
 def post_subst():
+    connection = database.newConnection()
+    cursor = database.newCursor(connection)
     x_version = request.headers.get('X-version')
     if x_version == '1':
         try:
             content = request.get_json()
-            database.createEntry(cursor, content['date'], content['class'], content['lesson'], content['teacher'], content['room'], content['notes'])
+            database.createEntry(cursor, *content)
             connection.commit()
             return '', 200
         except KeyError:
@@ -37,7 +40,7 @@ def post_subst():
     elif x_version == '2':
         try:
             content = request.get_json()
-            database.createEntry(cursor, content['date'], content['class'], content['lesson'], content['teacher'], content['room'], content['notes'])
+            database.createEntry(cursor, content['date'], content['class'], content['lesson'], content['subject'], content['teacher'], content['room'], content['notes'])
             connection.commit()
             return '', 200
         except KeyError:
@@ -47,14 +50,35 @@ def post_subst():
 
 @app.route('/subst', methods=['DELETE'])
 def delete_subst():
+    connection = database.newConnection()
+    cursor = database.newCursor(connection)
     try:
         ids = request.get_json()
         for id in ids:
             database.deleteEntry(cursor, id)
         connection.commit()
         return '', 200
-    except:
+    except KeyError: # not possible
         return '', 400
 
+@app.route('/subst', methods=['PUT'])
+def edit_subst():
+    connection = database.newConnection()
+    cursor = database.newCursor(connection)
+    x_version = request.headers.get('X-version')
+    content = request.get_json()
+    if x_version == "1":
+        try:
+            database.updateEntry(cursor, content[0], content[1:])
+            connection.commit()
+        except IndexError:
+            return '', 400
+    elif x_version == "2":
+        try:
+            database.updateEntry(cursor, content['id'], (content['date'], content['class'], content['lesson'], content['subject'], content['teacher'], content['room'], content['notes']))
+            connection.commit()
+        except IndexError:
+            return '', 400
+    return '', 200
 if __name__ == '__main__':
     app.run(port=8000)
